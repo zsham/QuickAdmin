5509e4a476325188f4f430edb43440c4d85d0386
z@Sham <67310100+zsham@users.noreply.github.com>
2024-05-09 11:30:26 +0800

    Update db_connect.php

--- root/db_connect.php
+++ root/db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('localhost','root','Pablo@1984@@','fbms_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('localhost','root','','_db')or die("Could not connect to mysql".mysqli_error($con));
